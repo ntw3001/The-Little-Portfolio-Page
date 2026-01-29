4f3d1f05b6cafc662940e92b04d57163ce73b033
Nick Westbrook <ntw3001@gmail.com>
2026-01-30 03:08:15 +0900

    made the http s

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect('https://' + req.headers.host + req.url);
+  }
+  next();
+});
+
 app.use(express.static(path.join(__dirname)));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
